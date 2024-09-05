7218bce7338f67728985279444733aa87b303220
Ramon Ives <ramonivesdev.com>
2024-09-05 00:05:34 -0300

    Nginx com Node.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const port = 3000
+
+const config = {
+  host: 'mysql',
+  user: 'root',
+  password: 'root',
+  database: 'fullcycle'
+};
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) VALUES 
+    ('Ramon'), 
+    ('Jane Doe'), 
+    ('Jhon Doe')`
+connection.query(sql)
+
+app.get('/', (req, res) => {
+
+    connection.query(`SELECT name FROM people`, (err, results) => {
+      if (err) throw err;
+
+      let response = '<h1>Full Cycle Rocks!</h1><ul>';
+      results.forEach(person => {
+        response += `<li>${person.name}</li>`;
+      });
+      response += '</ul>';
+
+      res.send(response);
+    });
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
